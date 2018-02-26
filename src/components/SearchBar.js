0c857a439043323102189690a45156a1b8c44ee3
Burcu Noyan <burcunoyan@gmail.com>
2018-02-25 22:11:42 -0500

    make stateful component; handleChange on search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return (
-    <div id="search" className="col-xs-12">
-      <input id="search-box" placeholder="Enter an artist name" autoFocus="true" />
-      <button id="search-btn" className="btn btn-lg">Search</button>
-    </div>
-  )
-};
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(ev) {
+    this.setState({ input: ev.target.value });
+  }
+
+  render() {
+    const { handleChange } = this;
+    const { input } = this.state;
+
+    return (
+      <form id="search" className="col-xs-12">
+        <input
+          onChange={ handleChange }
+          id="search-box"
+          placeholder="Enter an artist name"
+          autoFocus="true"
+        />
+        <button id="search-btn" className="btn btn-lg">Search</button>
+      </form>
+    );
+  }
+}
 
 export default SearchBar;

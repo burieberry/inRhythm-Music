141845d772720b7f237a5c0d1b714ddbd5e1168d
Burcu Noyan <burcunoyan@gmail.com>
2018-02-25 23:12:28 -0500

    submit search results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,23 +7,30 @@ class SearchBar extends Component {
     super(props);
     this.state = { input: '' };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(ev) {
     this.setState({ input: ev.target.value });
   }
 
+  handleSubmit(ev) {
+    ev.preventDefault();
+    this.props.fetchAlbums(this.state.input);
+  }
+
   render() {
-    const { handleChange } = this;
+    const { handleChange, handleSubmit } = this;
     const { input } = this.state;
 
     console.log('props in SearchBar', this.props);
     console.log('state in SearchBar', this.state);
 
     return (
-      <form id="search" className="col-xs-12">
+      <form id="search" onSubmit={ handleSubmit } className="col-xs-12">
         <input
           onChange={ handleChange }
+          name="search"
           id="search-box"
           placeholder="Enter an artist name"
           autoFocus="true"
@@ -34,11 +41,6 @@ class SearchBar extends Component {
   }
 }
 
-const mapState = (props) => {
-  console.log(props)
-  return {};
-}
-
 const mapDispatch = ({ fetchAlbums });
 
-export default connect(mapState, mapDispatch)(SearchBar);
+export default connect(null, mapDispatch)(SearchBar);

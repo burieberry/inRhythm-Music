fde071bacb0bb2f8605208c4d6462181559603e0
Burcu Noyan <burcunoyan@gmail.com>
2018-02-27 02:00:51 -0500

    save search term in url

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchAlbums } from '../store';
 
@@ -10,18 +11,32 @@ class SearchBar extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    let searchTerm = '';
+    const { search } = this.props.location;
+    console.log(search)
+    if (search) {
+      searchTerm = search.split('=')[1];
+    }
+    console.log(searchTerm);
+    this.props.fetchAlbums(searchTerm);
+  }
+
   handleChange(ev) {
     this.setState({ input: ev.target.value });
   }
 
   handleSubmit(ev) {
+    const { input } = this.state;
     ev.preventDefault();
-    this.props.fetchAlbums(this.state.input);
+    this.props.fetchAlbums(input);
+    this.props.history.push({ search: `?search=${ input }` });
   }
 
   render() {
     const { handleChange, handleSubmit } = this;
     const { input } = this.state;
+    console.log(this.props)
 
     return (
       <form id="search" onSubmit={ handleSubmit } className="col-xs-12">
@@ -42,4 +57,4 @@ const mapState = ({ albums }) => ({ albums });
 
 const mapDispatch = ({ fetchAlbums });
 
-export default connect(mapState, mapDispatch)(SearchBar);
+export default withRouter(connect(mapState, mapDispatch)(SearchBar));
